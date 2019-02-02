27243bdae1a31a69b0815b6f4341db099739296c
Akshat Bajpai <35187768+Akbonline@users.noreply.github.com>
2019-02-02 14:54:15 -0500

    v2.0

--- src/myVisitor.java
+++ src/myVisitor.java
@@ -1,5 +1,6 @@
 public class myVisitor extends gBaseVisitor<Object>{
     @Override public Object visitPrule(gParser.PruleContext ctx) {
+        System.out.println("Success");
 
         return visitChildren(ctx);
 
