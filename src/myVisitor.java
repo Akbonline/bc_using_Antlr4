c583c652d0e6f048e39683a04e5d6849feaf3b14
Halee Corbin <kuroyukihime@ufl.edu>
2019-02-08 09:14:36 -0500

    Removed old files

--- src/myVisitor.java
+++
@@ -1,8 +0,0 @@
-public class myVisitor extends gBaseVisitor<Object>{
-    @Override public Object visitPrule(gParser.PruleContext ctx) {
-        System.out.println("Success");
-
-        return visitChildren(ctx);
-
-    }
-}
